515fc7667803326087d7be9b79a2210e9658d504
Siarhei Ruban <dnmneon4uk@gmail.com>
2022-08-31 15:59:59 +0300

    feature:
    добавил задание N.17

---
+++ N.17/js/script.js
@@ -0,0 +1,52 @@
+var userString = prompt('Введите строку:')
+
+console.log('Введёная строка: \n' + userString);
+
+function vowels_arr_reduce(str) {
+	str = str.toLowerCase();
+	var userArr = str.split('');
+	var searchedVowels = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"];
+
+	function ff(r, v, i, a) {
+		if (searchedVowels.indexOf(v) !== -1) {
+			r++;
+		}
+		return r;
+	}
+
+	return userArr.reduce(ff, 0);
+}
+
+function vowels_arr_forEach(str) {
+	str = str.toLowerCase();
+	var userArr = str.split('');
+	var searchedVowels = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"];
+	var count = 0;
+
+	function f(v, i, a) {
+		if (searchedVowels.indexOf(v) !== -1) {
+			count++;
+		}
+	}
+
+	userArr.forEach(f);
+	return count;
+}
+
+function vowels_arr_filter(str) {
+	str = str.toLowerCase();
+	var userArr = str.split('');
+	var searchedVowels = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"];
+
+	function funct(v, i, a) {
+		if (searchedVowels.indexOf(v) !== -1) {
+			return true;
+		}
+	}
+	let filteredArr = userArr.filter(funct);
+
+	return filteredArr.length;
+}
+console.log('Количество гласных в строке методом forEach: \n' + vowels_arr_forEach(userString) + '\n' +
+	'Количество гласных в строке методом filter: \n' + vowels_arr_filter(userString) + '\n' +
+	'Количество гласных в строке методом reduce: \n' + vowels_arr_reduce(userString));
