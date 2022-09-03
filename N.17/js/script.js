d711b6aebf83a581ace46339d91e78f98b00f704
Siarhei Ruban <dnmneon4uk@gmail.com>
2022-09-03 09:23:59 +0300

    refactoring

--- N.17/js/script.js
+++ N.17/js/script.js
@@ -2,21 +2,6 @@ var userString = prompt('Введите строку:')
 
 console.log('Введёная строка: \n' + userString);
 
-function vowels_arr_reduce(str) {
-	str = str.toLowerCase();
-	var userArr = str.split('');
-	var searchedVowels = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"];
-
-	function ff(r, v, i, a) {
-		if (searchedVowels.indexOf(v) !== -1) {
-			r++;
-		}
-		return r;
-	}
-
-	return userArr.reduce(ff, 0);
-}
-
 function vowels_arr_forEach(str) {
 	str = str.toLowerCase();
 	var userArr = str.split('');
@@ -47,6 +32,22 @@ function vowels_arr_filter(str) {
 
 	return filteredArr.length;
 }
+
+function vowels_arr_reduce(str) {
+	str = str.toLowerCase();
+	var userArr = str.split('');
+	var searchedVowels = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"];
+
+	function ff(r, v, i, a) {
+		if (searchedVowels.indexOf(v) !== -1) {
+			r++;
+		}
+		return r;
+	}
+
+	return userArr.reduce(ff, 0);
+}
+
 console.log('Количество гласных в строке методом forEach: \n' + vowels_arr_forEach(userString) + '\n' +
 	'Количество гласных в строке методом filter: \n' + vowels_arr_filter(userString) + '\n' +
 	'Количество гласных в строке методом reduce: \n' + vowels_arr_reduce(userString));
