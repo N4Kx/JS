84301f21463b7f371d815d3251d03adefb9abce4
Siarhei Ruban <dnmneon4uk@gmail.com>
2022-10-17 15:40:34 +0300

    initial commit:
    написал часы на SVG

---
+++ N.37/js/script2.js
@@ -0,0 +1,239 @@
+'use strict'
+
+const bodyElem = document.body;	// находим элемент body, в нем будем строить часы
+const clockSizeElem = document.querySelector('#clockSize');	// находим поле ввода размера часов
+const clockBtn = document.querySelector('#clockBtn');	//находим кнопку
+let clockSize; // размер часов
+const clockBGColor = '#FCCB6E';	// цвет основы циферблата
+const numberBGColor = '#48B585';  // цвет кружочка под числом
+
+const arrowColor = 'black';	// задаем цвет стрелок
+
+
+clockBtn.addEventListener('click', getClockSize, false);	// вешаем обработчик событий на кнопку
+
+function getClockSize(eo) {
+	eo = eo || window.event;
+
+	if (clockSizeElem.value >= 200) {
+		clockSize = Number(clockSizeElem.value);
+		//===============================================================================================
+		// создаем часы (основа)
+		const clockRad = clockSize / 2;	// радиус часов
+
+		const clockCenterX = clockRad;	// координата центра часов по X
+		const clockCenterY = clockRad;	// координата центра часов по Y
+		const clockSvgBody = document.createElementNS('http://www.w3.org/2000/svg', 'svg');	// создаем тег SVG для рисования часов
+
+		clockSvgBody.setAttribute('width', clockSize);	// задаем ширину тега SVG
+		clockSvgBody.setAttribute('height', clockSize);	// задаем высоты тега SVG
+		bodyElem.prepend(clockSvgBody);	// помещаем тег SVG в начало тега body
+
+		const clock = document.createElementNS('http://www.w3.org/2000/svg', 'circle');	// создаем часы
+		clock.setAttribute('cx', clockCenterX);
+		clock.setAttribute('cy', clockCenterY);
+		clock.setAttribute('r', clockRad);
+		clock.setAttribute('fill', clockBGColor);
+
+		clockSvgBody.prepend(clock);
+
+		//===============================================================================================
+		// создаем циферблат
+		const hourDistance = clockRad * 0.8;	// задаем расстояние для циферок циферблата
+		const hourRad = clockRad * 0.1;	// задаём радиус кружочка для циферблата
+		const hourTextSize = clockSize / 20; // задаем размер шрифта относительно размера часов
+
+		for (let i = 1; i <= 12; i++) {
+
+			const hourAngle = 2 * Math.PI / 12 * i;	// вычисляем угол на котором будет расположен элемент циферблата
+
+			const hourX = clockCenterX + hourDistance * Math.sin(hourAngle);
+			const hourY = clockCenterY - hourDistance * Math.cos(hourAngle);
+
+			const hourBGElem = document.createElementNS('http://www.w3.org/2000/svg', 'circle');	// создаем кружочек
+			hourBGElem.setAttribute('cx', hourX);
+			hourBGElem.setAttribute('cy', hourY);
+			hourBGElem.setAttribute('r', hourRad);
+			hourBGElem.setAttribute('fill', numberBGColor);
+
+			clockSvgBody.append(hourBGElem);
+
+			const hourTextX = hourX;
+			const hourTextY = hourY + hourTextSize / 4;	// т.к. text-anchor = middle - выравниваем на 1/4 высоты шрифта
+
+			const hourTextElem = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+			hourTextElem.setAttribute('x', hourTextX);
+			hourTextElem.setAttribute('y', hourTextY);
+			hourTextElem.setAttribute('font-size', hourTextSize);
+			hourTextElem.setAttribute('text-anchor', 'middle');	// выравнивание текста по середине базовой линии текста
+
+			const hourText = document.createTextNode(i);
+			hourTextElem.appendChild(hourText);
+
+			clockSvgBody.append(hourTextElem);
+		}
+
+		//===============================================================================================
+		// создаем часовую стрелку
+		const hourArrowLength = clockRad * 0.5;	// длина часовой стрелки
+		const hourAnglethickness = 20;		// толщина часовой стрелки
+		// const hourArrowAngle = 0;
+		/*
+				const hourArrowX = clockCenterX + hourArrowLength * Math.sin(hourArrowAngle);
+				const hourArrowY = clockCenterY - hourArrowLength * Math.cos(hourArrowAngle);
+		
+				const hourArrow = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+				hourArrow.setAttribute('x1', clockCenterX);
+				hourArrow.setAttribute('y1', clockCenterY);
+				hourArrow.setAttribute('x2', hourArrowX);
+				hourArrow.setAttribute('y2', hourArrowY);
+				hourArrow.setAttribute('stroke', 'black');
+				hourArrow.setAttribute('stroke-width', 20);
+				hourArrow.setAttribute('stroke-linecap', 'round');
+		
+				clockSvgBody.append(hourArrow);
+		*/
+
+
+
+
+		// buildArrow(clockSvgBody, hourArrowLength, hourArrowAngle, 20, arrowColor, clockCenterX, clockCenterY);
+
+		//===============================================================================================
+		// создаем минутную стрелку
+		const minArrowLength = clockRad * 0.8;		// длина минутной стрелки
+		const minArrowthickness = 8;		// толщина минутной стрелки
+		// const minArrowAngle = 0;
+
+		/*
+		const minArrowX = clockCenterX + minArrowLength * Math.sin(minArrowAngle);
+		const minArrowY = clockCenterY - minArrowLength * Math.cos(minArrowAngle);
+
+		const minArrow = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+		minArrow.setAttribute('x1', clockCenterX);
+		minArrow.setAttribute('y1', clockCenterY);
+		minArrow.setAttribute('x2', minArrowX);
+		minArrow.setAttribute('y2', minArrowY);
+		minArrow.setAttribute('stroke', 'black');
+		minArrow.setAttribute('stroke-width', 8);
+		minArrow.setAttribute('stroke-linecap', 'round');
+
+		clockSvgBody.append(minArrow);
+*/
+		// buildArrow(clockSvgBody, minArrowLength, minArrowAngle, 8, arrowColor, clockCenterX, clockCenterY);
+
+		//===============================================================================================
+		// создаем секундную стрелку
+		const secArrowLength = clockRad * 0.95;		// длина секундной стрелки
+		const secArrowthickness = 2;		// толщина секундной стрелки
+		// const secArrowAngle = 0;
+
+		// const secArrowX = clockCenterX + secArrowLength * Math.sin(secArrowAngle);
+		// const secArrowY = clockCenterY - secArrowLength * Math.cos(secArrowAngle);
+
+		// const secArrow = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+		// secArrow.setAttribute('x1', clockCenterX);
+		// secArrow.setAttribute('y1', clockCenterY);
+		// secArrow.setAttribute('x2', secArrowX);
+		// secArrow.setAttribute('y2', secArrowY);
+		// secArrow.setAttribute('stroke', 'black');
+		// secArrow.setAttribute('stroke-width', 2);
+		// secArrow.setAttribute('stroke-linecap', 'round');
+
+		// clockSvgBody.append(secArrow);
+
+		// buildArrow(clockSvgBody, secArrowLength, secArrowAngle, 2, arrowColor, clockCenterX, clockCenterY)
+
+		//===============================================================================================
+		// создаем цифровые часы
+		const digitalClockDistance = clockRad * 0.32;	// смещение цифровых часов по оси Y
+		const digitalClockFontSize = 40;		// задаем размер шрифта цифровым часам
+
+		//===============================================================================================
+		// создаем функцию, обновляющую часы (стрелки, цифры)
+		function updateClock() {
+			const currTime = new Date();
+
+			const currHour = currTime.getHours();
+			const currMin = currTime.getMinutes();
+			const currSec = currTime.getSeconds();
+			const currMs = currTime.getMilliseconds();
+
+			const currentTime = str0l(currHour, 2) + ':' + str0l(currMin, 2) + ':' + str0l(currSec, 2);
+
+			const hourAngle = 2 * Math.PI / 12 * (currHour + currMin / 60);
+			const minArrowAngle = 2 * Math.PI / 60 * currMin;
+			const secArrowAngle = 2 * Math.PI / 60 * currSec;
+
+			while (clockSvgBody.querySelector('line')) {
+				clockSvgBody.querySelector('line').remove();
+			}
+			buildArrow(clockSvgBody, hourArrowLength, hourAngle, hourAnglethickness, arrowColor, clockCenterX, clockCenterY);
+			buildArrow(clockSvgBody, minArrowLength, minArrowAngle, minArrowthickness, arrowColor, clockCenterX, clockCenterY);
+			buildArrow(clockSvgBody, secArrowLength, secArrowAngle, secArrowthickness, arrowColor, clockCenterX, clockCenterY);
+
+			while (clockSvgBody.querySelector('text')) {
+				clockSvgBody.querySelector('text').remove();
+			}
+			buildDigitelClock(clockSvgBody, digitalClockDistance, digitalClockFontSize, clockCenterX, clockCenterY, currentTime);
+
+
+			setTimeout(updateClock, (1010 - currMs));
+
+			console.log(currentTime);
+		}
+
+		updateClock();
+
+		//===============================================================================================
+		// удаляем все "ненужные" элементы
+		bodyElem.querySelector('label').remove();	// удаляем label
+		clockSizeElem.remove();	// удаляем поле ввода размера
+		clockBtn.remove();	// удаляем кнопку
+		while (bodyElem.querySelector('br')) {
+			bodyElem.querySelector('br').remove();	// удаляем все теги
+		}
+	} else {
+		alert('Введите размер часов НЕ менее 200');
+		clockSizeElem.focus();
+	}
+}
+
+function buildArrow(body, length, angle, thickness, color, x1, y1) {
+	const arrowX = x1 + length * Math.sin(angle);
+	const arrowY = y1 - length * Math.cos(angle);
+
+	const arrow = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+
+	arrow.setAttribute('x1', x1);
+	arrow.setAttribute('y1', y1);
+	arrow.setAttribute('x2', arrowX);
+	arrow.setAttribute('y2', arrowY);
+	arrow.setAttribute('stroke', color);
+	arrow.setAttribute('stroke-width', thickness);
+	arrow.setAttribute('stroke-linecap', 'round');
+
+	body.append(arrow);
+}
+
+function buildDigitelClock(body, distance, fontSize, x0, y0, time) {
+	const digitalClock = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+
+	digitalClock.setAttribute('x', x0);
+	digitalClock.setAttribute('y', y0 - distance);
+	digitalClock.setAttribute('text-anchor', 'middle');
+	digitalClock.setAttribute('font-size', fontSize);
+	digitalClock.setAttribute('id', 'DIGITAL_CLOCK');
+
+	let digitalClockText = document.createTextNode(time);
+	digitalClock.appendChild(digitalClockText);
+	body.append(digitalClock);
+}
+
+// дополняет строку val слева нулями до длины Len
+function str0l(val, len) {
+	let strVal = val.toString();
+	while (strVal.length < len)
+		strVal = '0' + strVal;
+	return strVal;
+}
